73c083e008685e5cf396e4c4e1d3795af79b28f6
m <dev.cheuk.lee@gmail.com>
2020-01-30 18:59:10 -0500

    added basic questionaire ui

--- src/app/views/questionaire/questionaire.component.ts
+++ src/app/views/questionaire/questionaire.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import questions from '../../seed/data.json';
 
 @Component({
   selector: 'app-questionaire',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionaire.component.css']
 })
 export class QuestionaireComponent implements OnInit {
+  questionForm = this.fb.group({
+    yesNo: '',
+    written: '',
+    checklist: ''
+  });
+  questions;
+  section_num = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { 
+    this.questions = questions.filter((q, i) => q.section_num === this.section_num);
+    this.section_num = 1;
   }
 
+  ngOnInit() {
+    this.questionForm.valueChanges.subscribe(console.log);
+  }
 }
