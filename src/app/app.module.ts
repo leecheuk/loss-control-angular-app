188867884e7322bbb7895af4a291ab1bbfb25c20
m <dev.cheuk.lee@gmail.com>
2020-01-29 22:46:15 -0500

    added report view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './views/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuestionaireComponent } from './views/questionaire/questionaire.component';
 import { ReviewComponent } from './views/review/review.component';
+import { ReportComponent } from './views/report/report.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ReviewComponent } from './views/review/review.component';
     HomeComponent,
     NavbarComponent,
     QuestionaireComponent,
-    ReviewComponent
+    ReviewComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
