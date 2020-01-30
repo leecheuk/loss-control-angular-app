a29e671528834c0db5d070b2a96a8ca25500dbcd
m <dev.cheuk.lee@gmail.com>
2020-01-30 02:47:21 -0500

    added questionaire route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './views/home/home.component';
+import {QuestionaireComponent} from './views/questionaire/questionaire.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'questionaire', component: QuestionaireComponent }
 ];
 
 @NgModule({
