2ec0b515df5fd90bebd49528492aba7d91a9f7d2
m <dev.cheuk.lee@gmail.com>
2020-01-29 22:45:47 -0500

    added review view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuestionaireComponent } from './views/questionaire/questionaire.component';
+import { ReviewComponent } from './views/review/review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    QuestionaireComponent
+    QuestionaireComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
