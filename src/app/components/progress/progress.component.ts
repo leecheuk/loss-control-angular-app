f9381320a9427a82117a78db194b82a24c1d2c43
m <dev.cheuk.lee@gmail.com>
2020-02-01 21:19:17 -0500

    removed duplicate import

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit, ViewChildren } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
