f61307206ec46ea1c9accd73e3d4221022c55f39
m <dev.cheuk.lee@gmail.com>
2020-02-03 00:50:35 -0500

    updated response model doc

--- src/app/models/response.ts
+++ src/app/models/response.ts
@@ -1,7 +1,12 @@
 /**
  * A model for response of each question.
  * @typedef {object} Response
- * @property {}
+ * @property {string} Response.id - Unique primary key
+ * @property {number} Response.num - Number corresponding to question number
+ * @property {number} Response.section_num - Section number of corresponding question
+ * @property {string | null} Response.yesNo - User response/input to yes or no option
+ * @property {string | null} Response.written - User response/input to question
+ * @property {boolean[]} Response.checklist - Indicator of whether checklist items are checked or not
  */
 
  export interface Response {
@@ -10,5 +15,5 @@
      section_num: number;
      yesNo: string | null;
      written: string | null;
-     checklist: string[];
+     checklist: boolean[];
  }
